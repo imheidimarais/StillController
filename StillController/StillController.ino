b0101a4e7113a2169d3add6eb082e6be006a4396
DESKTOP-1T8IPU7\imheidi <imheidimarais@gmail.com>
2018-03-28 19:34:57 +0200

    Implemented program flow
    Flow diagram logic mostly implemented.
    Error logging logic not yet done.

--- StillController/StillController.ino
+++ StillController/StillController.ino
@@ -1,11 +1,75 @@
 #define BAUDRATE 9600
+#define MEASUREMENTFAIL -126F  // functions will return -127 for an error
+#define N_MAX 10 // total number of measurements
 
 void setup() {
-  // put your setup code here, to run once:
+  // initialisation step
   Serial.begin(BAUDRATE)
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //1. wait for data on the serial bus
 
+  byte cmd;
+  switch(cmd){
+    case 0:
+      // do thing associated with cmd0
+    case 1:
+      // start measurement loop
+      measurement_loop();
+  }
 }
+
+byte measurement_loop(){
+  // check for data on serial bus
+  // if no data coms false, if data coms true
+  for(byte N = 0; ; N+=1){
+    byte coms;
+    coms = false;
+  
+    if(coms){
+      // handle this
+      break;
+    }
+    else{
+      
+      // work through sensors
+      measure_sensor(N);
+      if(N == N_MAX){
+        N = 0;
+      }
+    }
+  }
+}
+
+byte measure_sensor(N){
+  // example structure
+  float measurement;
+  
+  if(1 <= N <= 4){
+    measurement = measure_temperature_type1(N);
+    
+  }
+  else if(4 < N <=6){
+    measurement = measure_temperature_type2(N);
+  }
+
+ // log
+ if(measurement <= MEASUREMENTFAIL){
+  // call log error
+ }
+ else{
+  // call log measurement
+ }
+ // return to measurement loop
+}
+
+byte log_measurement(){
+  
+}
+
+byte log_error(){
+  
+}
+
